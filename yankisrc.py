ea55bb257306edd50ed53b7f069e0bc7bbb367fb
Ian Weller <ian@ianweller.org>
2012-05-01 15:45:01 -0500

    Add license text

--- yankisrc.py
+++ yankisrc.py
@@ -1,3 +1,9 @@
+# (c) 2012 Ian Weller
+# This program is free software. It comes without any warranty, to the extent
+# permitted by applicable law. You can redistribute it and/or modify it under
+# the terms of the Do What The Fuck You Want To Public License, Version 2, as
+# published by Sam Hocevar. See COPYING for more details.
+
 import codecs
 from datetime import datetime
 from getpass import getpass
